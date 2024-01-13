598691d1141e049e648be6f7c9d3fa901b55444a
Sergei Sokolov <sergsok1984@gmail.com>
2024-01-13 09:19:24 +0300

    add HW_2 to Lesson_2

---
+++ Lesson_2/src/main/java/Main.java
@@ -0,0 +1,51 @@
+public class Main {
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 7;
+        int b = -6;
+
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 101;
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value <= 100) {
+            System.out.println("Жёлтый");
+        } else {
+            System.out.println("Зелёный");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 15;
+        int b = 43;
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+    public static void main(String[] args) {
+        printThreeWords();
+        System.out.println();
+        checkSumSign();
+        System.out.println();
+        printColor();
+        System.out.println();
+        compareNumbers();
+    }
+}
