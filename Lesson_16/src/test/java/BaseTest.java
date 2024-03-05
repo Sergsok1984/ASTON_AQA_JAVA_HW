4e7cd410b5543107daa215297047d92cebc1b259
Sergei Sokolov <sergsok1984@gmail.com>
2024-03-05 10:47:02 +0300

    add HW_16 Lesson_16

--- Lesson_16/src/test/java/BaseTest.java
+++ Lesson_16/src/test/java/BaseTest.java
@@ -1,6 +1,7 @@
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
+
 import static io.restassured.RestAssured.expect;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
@@ -8,31 +9,15 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class BaseTest {
 
-    protected RequestSpecification requestSpec1 = given()
+    protected RequestSpecification requestSpec = given()
             .baseUri("https://postman-echo.com");
 
-    protected ResponseSpecification responseSpec1 = expect()
+    protected ResponseSpecification responseSpec = expect()
             .statusCode(200)
             .contentType(ContentType.JSON)
             .and().body("headers.x-forwarded-proto", equalTo("https"))
             .and().body("headers.x-forwarded-port", equalTo("443"))
             .and().body("headers.host", equalTo("postman-echo.com"))
-            .and().body("headers.x-amzn-trace-id", containsString("Root="))
-            .and().body("headers.accept", equalTo("*/*"))
-            .and().body("headers.user-agent", containsString("Apache-HttpClient"))
-            .and().body("headers.accept-encoding", equalTo("gzip,deflate"));
-    protected RequestSpecification requestSpec2 = given()
-            .baseUri("https://postman-echo.com")
-            .body("Some raw text")
-            .when();
-
-    protected ResponseSpecification responseSpec2 = expect()
-            .statusCode(200)
-            .contentType(ContentType.JSON)
-            .and().body("headers.x-forwarded-proto", equalTo("https"))
-            .and().body("headers.x-forwarded-port", equalTo("443"))
-            .and().body("headers.host", equalTo("postman-echo.com"))
-            .and().body("headers.content-length", equalTo("13"))
             .and().body("headers.x-amzn-trace-id", containsString("Root="))
             .and().body("headers.accept", equalTo("*/*"))
             .and().body("headers.user-agent", containsString("Apache-HttpClient"))
--- Lesson_16/src/test/java/PostmanTest.java
+++ Lesson_16/src/test/java/PostmanTest.java
@@ -5,37 +5,46 @@ import static org.hamcrest.Matchers.equalTo;
 public class PostmanTest extends BaseTest {
     @Test
     public void getPostmanTest() {
-       requestSpec1
+       requestSpec
                 .get("/get")
                 .then().log().body()
-                .spec(responseSpec1)
+                .spec(responseSpec)
                 .and().body("url", equalTo("https://postman-echo.com/get"));
     }
 
     @Test
     public void postPostmanTest() {
-        requestSpec2
+        requestSpec
+                .body("Some raw text")
+                .when()
                 .post("/post")
                 .then().log().body()
-                .spec(responseSpec2)
+                .spec(responseSpec)
+                .and().body("headers.content-length", equalTo("13"))
                 .and().body("url", equalTo("https://postman-echo.com/post"));
     }
 
     @Test
     public void putPostmanTest() {
-        requestSpec2
+        requestSpec
+                .body("Some raw text")
+                .when()
                 .put("/put")
                 .then().log().body()
-                .spec(responseSpec2)
+                .spec(responseSpec)
+                .and().body("headers.content-length", equalTo("13"))
                 .and().body("url", equalTo("https://postman-echo.com/put"));
     }
 
     @Test
     public void deletePostmanTest() {
-        requestSpec2
+        requestSpec
+                .body("Some raw text")
+                .when()
                 .delete("/delete")
                 .then().log().body()
-                .spec(responseSpec2)
+                .spec(responseSpec)
+                .and().body("headers.content-length", equalTo("13"))
                 .and().body("url", equalTo("https://postman-echo.com/delete"));
     }
 }
